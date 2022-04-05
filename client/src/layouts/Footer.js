ef870ecd2cd7bd287cf8b0dde5f03293b05bd1ac
Ziek <eas260@txstate.edu>
2022-04-05 00:08:49 -0500

    corrected links in footer

--- client/src/layouts/Footer.js
+++ client/src/layouts/Footer.js
@@ -1,8 +1,8 @@
 function Footer() {
     return (
         <footer className="d-flex justify-content-center mt-5">
-            <p>Icons By <a target="_blank" rel="noreferrer" href="https://icons8">Icons8</a> | Illustration by <a
-                target="_blank" rel="noreferrer" href="https://icons8">ManyPixels</a>
+            <p>Icons By <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a> | Illustration by <a
+                target="_blank" rel="noreferrer" href="https://freewebillustrations.com/illustrations-gallery-american-football/">ManyPixels</a>
             </p>
         </footer>
     );
